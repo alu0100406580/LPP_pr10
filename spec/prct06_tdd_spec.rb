68ad742ac51e95811a57d9f9cdbe52eeef58d558
Dario Afonso Rodriguez <alu0100406580@ull.edu.es>
2019-11-10 02:18:05 +0000

    pruebas creación de alimentos de manera correcta

--- spec/prct06_tdd_spec.rb
+++ spec/prct06_tdd_spec.rb
@@ -3,7 +3,20 @@ RSpec.describe Prct06Tdd do
     expect(Prct06Tdd::VERSION).not_to be nil
   end
 
+  before :each do
+    @chocolate = Alimento.new( 5.3, 47.0, 30.0, 2.3, 3.4)
+    @pollo = Alimento.new( 20.6, 0.0, 5.6, 5.7, 7.1)
+    @queso = Alimento.new( 25.0, 1.3, 33.0, 11.0, 41.0)
+  end
+
   it "Creamos la clase Alimento" do
     expect( Alimento.new() ).not_to eq( nil )
   end
+
+  it "Creamos los alimentos de manera correcta" do
+    expect(@chocolate).not_to eq(nil)
+    expect(@pollo).not_to eq(nil)
+    expect(@queso).not_to eq(nil)
+  end
+
 end
